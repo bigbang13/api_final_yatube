a317a36c7d43af74c789e411cca27e2111520743
Василий Рябов <ryabov.v.s@yandex.ru>
2022-03-25 10:55:02 +0300

    api_final_v1

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,13 @@
+from posts.models import Comment, Follow, Group, Post, User
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
 
-from posts.models import Comment, Post
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = '__all__'
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -17,7 +22,25 @@ class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         read_only=True, slug_field='username'
     )
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         fields = '__all__'
         model = Comment
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = SlugRelatedField(
+        queryset=User.objects.all(),
+        default=serializers.CurrentUserDefault(),
+        slug_field='username'
+    )
+    following = SlugRelatedField(queryset=User.objects.all(), slug_field='username')
+
+    class Meta:
+        fields = ('user', 'following')
+        model = Follow
+    
+    def validate(self, data):
+        if data["user"] == data["following"]:
+            raise serializers.ValidationError("Нельзя подписываться на себя!")
+        return data
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,87 @@
-# TODO:  Напишите свой вариант
+import datetime as dt
+
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import get_object_or_404
+from posts.models import Follow, Group, Post
+from rest_framework import filters, viewsets
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import (IsAuthenticated,
+                                        IsAuthenticatedOrReadOnly)
+
+from .permissions import IsOwnerOrReadOnly
+from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
+                          PostSerializer)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(
+                author=self.request.user,
+                pub_date=dt.datetime.now()
+            )
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Нельзя изменять чужой пост!')
+        super(PostViewSet, self).perform_update(serializer)
+
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user:
+            raise PermissionDenied('Нельзя удалять чужой пост!')
+        super(PostViewSet, self).perform_destroy(instance)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        return post.comments.all()
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(
+                author=self.request.user,
+                post=get_object_or_404(Post, pk=self.kwargs.get('post_id')),
+                created=dt.datetime.now()
+            )
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Нельзя изменять чужой коммент!')
+        super(CommentViewSet, self).perform_update(serializer)
+
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user:
+            raise PermissionDenied('Нельзя удалять чужой коммент!')
+        super(CommentViewSet, self).perform_destroy(instance)
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = FollowSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('following__username',)
+
+
+    def get_queryset(self):
+        queryset = Follow.objects.filter(user=self.request.user)
+        return queryset
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(
+                user=self.request.user,
+            )
